373a55b716d585fd275e55fb25da50ff77fb6479
matemahinkhan <tutenstine@gmail.com>
2018-11-12 11:02:49 +0500

    added batch script

---
+++ backend/bin/configure_db.bat
@@ -0,0 +1,13 @@
+
+echo "Configuring dragonstackdb"
+
+
+dropdb -U node_user dragonstackdb
+createdb -U node_user dragonstackdb
+
+psql -U node_user dragonstackdb < .\\bin\\sql\\generation.sql
+psql -U node_user dragonstackdb < .\\bin\\sql\\dragon.sql
+
+
+echo "Configuration complete dragonstackdb"
+cmd /k
